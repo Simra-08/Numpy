efa60230e9cc4022b16b4680571f9545005dbc6c
Simra Shahid <simrashahid7@gmail.com>
2026-09-03 13:20:30 +0530

    numpy practice day 1

---
+++ Numpy_Practice/numpy_practice.py
@@ -0,0 +1,85 @@
+import numpy as np
+# print(np.__version__)
+
+
+# ---- INTRO -----
+# array = np.array([1,2,3])
+# array = array * 2
+# print(array)
+
+# ----- DIMENSIONS -----
+
+# array = np.array('a')  --- 0D
+# array = np.array([1,2,3]) ---- 1D
+# array = np.array([ [1,2,3],[4,5,6] ]) --- 3D
+# array = np.array([ [[1,2,3],[4,5,6]],
+#                   [[1,2,3],[4,5,6]],
+#                   [[1,2,3],[4,5,6]] ])
+# print(array.ndim)
+
+
+# ---- ACCESSING ------
+# array = np.array([ [['A','B','C'],['D','E','F'],['G','H','I']],
+#                     [['J','K','L'],['M','N','O'],['P','Q','R']],
+#                     [['S','T','U'],['V','W','X'],['Y','Z',' ']] ])
+# print(array.shape)
+# word = array[0,0,0] + array[2,0,0] + array[2,0,0]
+# print(word)
+
+
+# ------ SLICING ----------------
+# array = np.array([ [1,2,3,4],
+#                   [5,6,7,8],
+#                   [9,10,11,12],
+#                   [13,14,15,16] ])
+# # print(array[1:2])
+# print(array[:,-4])
+
+
+# ---------- DAY 1 PRACTICE -----------------------------------------------
+# import numpy as np
+# array = np.array([1,2,3,4,5])
+# print(array.ndim)
+# print(array.shape)
+# print(array.size)
+# print(array.dtype)
+
+
+# array = np.array([   [1,2,3],
+#                      [4,5,6]   ])
+# print(array.ndim)
+# print(array.shape)
+# print(array.dtype)
+
+
+# array = np.array([ [[1,2,3],[4,5,6]],
+#                     [[1,2,3],[4,5,6]] 
+#                 ])
+# print(array.ndim)
+# print(array.shape)
+# print(array.size)
+# print(array.dtype)
+
+# array = np.array([1.5,2,5,3,5])
+# print(array.dtype)
+
+# array = np.array([ [10, 20], [30, 40], [50, 60] ])
+# print(array.ndim)
+# print(array.shape)
+# print(array.size)
+
+# array = np.array([1.0,2,3,4,5,6,7,8,9,10])
+
+# print(array.dtype)
+
+
+# array = np.array([  [1,2,3,4],[5,6,7,8],[9,10,11,12]    ])
+# print(array.ndim)
+# print(array.shape)
+# print(array.size)
+
+# my_list = [1,2,3,4,5]
+# my_array = np.array(my_list)
+# print(type(my_array))
+# print(my_array.ndim)
+
