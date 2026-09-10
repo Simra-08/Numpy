164012b321ed6b32a1ef441319cecc3b4b444180
Simra Shahid <simrashahid7@gmail.com>
2026-09-10 18:35:44 +0530

    update

--- Numpy_Practice/numpy_practice.py
+++ Numpy_Practice/numpy_practice.py
@@ -33,7 +33,7 @@ import numpy as np
 #                   [9,10,11,12],
 #                   [13,14,15,16] ])
 # # print(array[1:2])
-# print(array[:,-4])
+# print(array[:,-1])
 
 
 # ---------- DAY 1 PRACTICE -----------------------------------------------
@@ -83,3 +83,10 @@ import numpy as np
 # print(type(my_array))
 # print(my_array.ndim)
 
+
+array = np.array([[1,2,3,4],
+                  [5,6,7,8],
+                  [9,10,11,12],
+                  [13,14,15,16]])
+print(array[:,1:])
+print(array[:,::2])
